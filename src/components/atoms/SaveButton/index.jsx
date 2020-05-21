38caed0b0be5f00806050a3d18965c3517075e06
Christian Aichner <contact@aichner-christian.com>
2020-05-21 19:14:12 +0200

    Add Head prototype and add change handling
    You can now edit the head and locally save the changes.

---
+++ src/components/atoms/SaveButton/index.jsx
@@ -0,0 +1,47 @@
+//#region > Imports
+//> React
+// Contains all the functionality necessary to define React components
+import React from "react";
+// React Prop Types
+import PropTypes from "prop-types";
+
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import { MDBBtn, MDBIcon } from "mdbreact";
+//#endregion
+
+//#region > Components
+class SaveButton extends React.Component {
+  render() {
+    const { value, initialValue } = this.props;
+
+    if (value !== initialValue) {
+      return (
+        <MDBBtn color="success" size="sm" onClick={this.props.handleSave}>
+          <MDBIcon icon="check-circle" />
+          Save
+        </MDBBtn>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+//#endregion
+
+//#region > PropTypes
+SaveButton.propTypes = {
+  value: PropTypes.string.isRequired,
+  initialValue: PropTypes.string.isRequired,
+  handleSave: PropTypes.func.isRequired,
+};
+//#endregion
+
+//#region > Exports
+export default SaveButton;
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2020 Werbeagentur Christian Aichner
+ */
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,12 +1,12 @@
 //> Components
 // Import all components to export them for easy access from parent components
 import AIButton from "./AIButton";
+import AIHead from "./AIHead";
+import SaveButton from "./SaveButton";
 
-export {
-  AIButton,
-};
+export { AIButton, AIHead, SaveButton };
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
  * Copyright © 2020 Werbeagentur Christian Aichner
  */
